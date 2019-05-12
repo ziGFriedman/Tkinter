7f96b650288b4c28c0e82c222ccecfb8353244db
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-05-12 19:03:59 +0500

    Create Colors.py

---
+++ Colors.py
@@ -0,0 +1,36 @@
+from tkinter import *
+
+class Color:
+
+    color = {
+        '#ff0000' : 'красный',
+        '#ff7d00' : 'оранжевый',
+        '#ffff00' : 'желтый',
+        '#00ff00' : 'зеленый',
+        '#007dff' : 'голубой',
+        '#0000ff' : 'синий',
+        '#7d00ff' : 'фиолетовый',
+    }
+
+    def __init__(self, master):
+        self.label = Label(master, text = 'нажми на кнопку', anchor = 'c')
+        self.label.pack()
+        self.entry = Entry(master, justify = CENTER)
+        self.entry.pack()
+        for key, value in self.color.items():
+            exec('Button(master, width = 18, command = self.key{2}value, bg = "{0}", text = "{1}").pack()'.format(key, value, key[1:]))    # exec - динамически генерируемый код
+
+    for key, value in color.items():
+        def bindedfunc(self, v = value, k = key):
+            self.label["text"] = v
+            self.entry.delete(0, END)
+            self.entry.insert(0, "{}".format(k))
+
+        exec('key{}value = bindedfunc'.format(key[1:]))
+
+root = Tk()
+root.geometry("200x245")
+root.title("Радуга цвета")
+app = Color(root)
+
+root.mainloop()
