7675daf65aedcc75aaa6d3a94badf87f9f23a64c
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-02 22:45:45 +0500

    Update Canvas_tags.py

--- Canvas_tags.py
+++ Canvas_tags.py
@@ -9,14 +9,14 @@ from tkinter import *
 
 root = Tk()
 
-c = Canvas(width = 200, height = 200, bg = 'white')
+c = Canvas(width=200, height=200, bg='white')
 c.pack()
 
-oval = c.create_oval(30, 10, 130, 80,tag = 'group1')
-c.create_line(10, 100, 450, 100, tag = 'group1')
+oval = c.create_oval(30, 10, 130, 80,tag='group1')
+c.create_line(10, 100, 450, 100, tag='group1')
 
 def color(event):
-     c.itemconfig('group1', fill = 'red', width = 3)
+     c.itemconfig('group1', fill='red', width=3)
 
 c.bind('<Button-3>', color)
 
@@ -29,24 +29,24 @@ root.mainloop()
 
 from tkinter import *
 
-c = Canvas(width = 460, height = 100, bg = 'grey80')
+c = Canvas(width=460, height=100, bg='grey80')
 c.pack()
 
-oval = c.create_oval(30, 10, 130, 80, fill = 'orange')
-c.create_rectangle(180, 10, 280, 80, tag = 'rect', fill = 'lightgreen')
-trian = c.create_polygon(330, 80, 380, 10, 430, 80, fill = 'white', outline = 'black')
+oval = c.create_oval(30, 10, 130, 80, fill='orange')
+c.create_rectangle(180, 10, 280, 80, tag='rect', fill='lightgreen')
+trian = c.create_polygon(330, 80, 380, 10, 430, 80, fill='white', outline='black')
 
 def oval_func(event):
      c.delete(oval)
-     c.create_text(80, 50, text = 'Круг')
+     c.create_text(80, 50, text='Круг')
 
 def rect_func(event):
      c.delete('rect')
-     c.create_text(230, 50, text = 'Прямоугольник')
+     c.create_text(230, 50, text='Прямоугольник')
 
 def triangle(event):
      c.delete(trian)
-     c.create_text(380, 50, text = 'Треугольник')
+     c.create_text(380, 50, text='Треугольник')
 
 c.tag_bind(oval, '<Button-1>', oval_func)
 c.tag_bind('rect', '<Button-1>', rect_func)
