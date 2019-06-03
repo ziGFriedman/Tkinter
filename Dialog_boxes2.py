711756ca3a371a0131abdbd926485ceadeada1a5
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-03 11:58:15 +0500

    Update Dialog_boxes2.py

--- Dialog_boxes2.py
+++ Dialog_boxes2.py
@@ -15,20 +15,21 @@ def insertText():
     f.close()
 
 def extractText():
-    file_name = fd.asksaveasfilename(filetypes = (('TXT files', '*.txt'), ('HTML files', '*.html;*.htm'), ('All files', '*.*')))
+    file_name = fd.asksaveasfilename(filetypes=(('TXT files', '*.txt'), (
+        'HTML files', '*.html;*.htm'), ('All files', '*.*')))
     f = open(file_name, 'w')
     s = text.get(1.0, END)
     f.write(s)
     f.close()
 
 root = Tk()
-text = Text(width = 50, height = 25)
-text.grid(columnspan = 2)
+text = Text(width=50, height=25)
+text.grid(columnspan=2)
 
-b1 = Button(text = 'Открыть', command = insertText)
-b1.grid(row = 1, sticky = E)
-b2 = Button(text = 'Сохранить', command = extractText)
-b2.grid(row = 1, column = 1, sticky = W)
+b1 = Button(text='Открыть', command=insertText)
+b1.grid(row=1, sticky=E)
+b2 = Button(text='Сохранить', command=extractText)
+b2.grid(row=1, column=1, sticky=W)
 
 root.mainloop()
 
