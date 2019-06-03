7b59d8996112e427e1a470584e29598f5de3bce7
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-03 11:58:03 +0500

    Update Menu_widget.py

--- Menu_widget.py
+++ Menu_widget.py
@@ -12,10 +12,10 @@ from tkinter import *
 
 root = Tk()
 mainmenu = Menu(root)
-root.config(menu = mainmenu)
+root.config(menu=mainmenu)
 
-mainmenu.add_command(label = 'Файл')
-mainmenu.add_command(label = 'Справка')
+mainmenu.add_command(label='Файл')
+mainmenu.add_command(label='Справка')
 
 root.mainloop()
 
@@ -33,20 +33,20 @@ from tkinter import *
 root = Tk()
 
 mainmenu = Menu(root)
-root.config(menu = mainmenu)
+root.config(menu=mainmenu)
 
-filemenu = Menu(mainmenu, tearoff = 0)
-filemenu.add_command(label = 'Открыть...')
-filemenu.add_command(label = 'Новый')
-filemenu.add_command(label = 'Сохранить...')
-filemenu.add_command(label = 'Выход')
+filemenu = Menu(mainmenu, tearoff=0)
+filemenu.add_command(label='Открыть...')
+filemenu.add_command(label='Новый')
+filemenu.add_command(label='Сохранить...')
+filemenu.add_command(label='Выход')
 
-helpmenu = Menu(mainmenu, tearoff = 0)
-helpmenu.add_command(label = 'Помощь')
-helpmenu.add_command(label = 'О программе')
+helpmenu = Menu(mainmenu, tearoff=0)
+helpmenu.add_command(label='Помощь')
+helpmenu.add_command(label='О программе')
 
-mainmenu.add_cascade(label = 'Файл', menu = filemenu)
-mainmenu.add_cascade(label = 'Справка', menu = helpmenu)
+mainmenu.add_cascade(label='Файл', menu=filemenu)
+mainmenu.add_cascade(label='Справка', menu=helpmenu)
 
 root.mainloop()
 
