fcaba38e3436e9132f3d3469d9fbb70918796253
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-04 13:33:50 +0500

    Update Method_canvas.py

--- Method_canvas.py
+++ Method_canvas.py
@@ -11,15 +11,15 @@ from tkinter import *
 
 root = Tk()
 
-c = Canvas(root, width = 200, height = 200, bg = 'white')
+c = Canvas(root, width=200, height=200, bg='white')
 c.pack()
 
 c.create_line(10, 10, 190, 50)
 
-c.create_line(100, 180, 100, 60, fill = 'green',
-                width = 5, arrow = LAST, dash = (10,2),
-                activefill = 'lightgreen',
-                arrowshape = "10 20 10")
+c.create_line(100, 180, 100, 60, fill='green',
+                width=5, arrow=LAST, dash=(10,2),
+                activefill='lightgreen',
+                arrowshape="10 20 10")
 
 root.mainloop()
 
@@ -72,14 +72,14 @@ from tkinter import *
 
 root = Tk()
 
-c = Canvas(root, width = 200, height = 200, bg = 'white')
+c = Canvas(root, width=200, height=200, bg='white')
 c.pack()
 
-c.create_oval(10, 10, 190, 190, fill = 'lightgrey', outline = 'white')
-c.create_arc(10, 10, 190, 190, start = 0, extent = 45, fill = 'red')
-c.create_arc(10, 10, 190, 190, start = 180, extent = 25, fill = 'orange')
-c.create_arc(10, 10, 190, 190, start = 240, extent = 100, style = CHORD, fill = 'green')
-c.create_arc(10, 10, 190, 190, start = 160, extent = -70, style = ARC, outline = 'darkblue', width = 5)
+c.create_oval(10, 10, 190, 190, fill='lightgrey', outline='white')
+c.create_arc(10, 10, 190, 190, start=0, extent=45, fill='red')
+c.create_arc(10, 10, 190, 190, start=180, extent=25, fill='orange')
+c.create_arc(10, 10, 190, 190, start=240, extent=100, style=CHORD, fill='green')
+c.create_arc(10, 10, 190, 190, start=160, extent=-70, style=ARC, outline='darkblue', width=5)
 
 root.mainloop()
 
@@ -87,10 +87,10 @@ root.mainloop()
 
 '''На холсте можно разместить текст. Делается это с помощью метода create_text():'''
 …
-c.create_text(100, 100, text = "Hello World,\nPython\nand Tk",
-                justify = CENTER, font = "Verdana 14")
-c.create_text(200, 200, text = "About this",
-                anchor = SE, fill = "grey")
+c.create_text(100, 100, text="Hello World,\nPython\nand Tk",
+                justify=CENTER, font="Verdana 14")
+c.create_text(200, 200, text="About this",
+                anchor=SE, fill="grey")
 …
 
 '''По умолчанию в заданной координате располагается центр текстовой надписи. Чтобы
