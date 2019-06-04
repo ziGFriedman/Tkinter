2966b54ba66be624fadfde55bba93c9af4c74d0b
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-04 13:33:40 +0500

    Update Method_place.py

--- Method_place.py
+++ Method_place.py
@@ -29,8 +29,8 @@ from tkinter import *
 root = Tk()
 root.geometry('150x150')
 
-Button(bg = 'red').place(x = 75, y = 20)
-Button(bg = 'green').place(relx = 0.3, rely = 0.5)
+Button(bg='red').place(x=75, y=20)
+Button(bg='green').place(relx=0.3, rely=0.5)
 
 root.mainloop()
 
