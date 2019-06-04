ca68964b34bbc6417bbe05f4ca5affd829d88bb7
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-04 13:33:48 +0500

    Update Method_grid.py

--- Method_grid.py
+++ Method_grid.py
@@ -25,20 +25,20 @@ from tkinter import *
 
 root = Tk()
 
-Label(text = 'Имя:').grid(row = 0, column = 0)
-table_name = Entry(width = 30)
-table_name.grid(row = 0, column = 1, columnspan = 3)
+Label(text='Имя:').grid(row=0, column=0)
+table_name = Entry(width=30)
+table_name.grid(row=0, column=1, columnspan=3)
 
-Label(text = 'Столбцов:').grid(row = 1, column = 0)
-table_column = Spinbox(width = 7, from_ = 1, to = 50)
-table_column.grid(row = 1, column = 1)
-Label(text = 'Строк:').grid(row = 1, column = 2)
+Label(text='Столбцов:').grid(row=1, column=0)
+table_column = Spinbox(width=7, from_=1, to=50)
+table_column.grid(row=1, column=1)
+Label(text='Строк:').grid(row=1, column=2)
 table_row = Spinbox(width = 7, from_ = 1, to = 100)
 table_row.grid(row = 1, column = 3)
 
-Button(text = 'Справка').grid(row = 2, column = 0)
-Button(text = 'Вставить').grid(row = 2, column = 2)
-Button(text = 'Отменить').grid(row = 2, column = 3)
+Button(text='Справка').grid(row=2, column=0)
+Button(text='Вставить').grid(row=2, column=2)
+Button(text='Отменить').grid(row=2, column=3)
 
 root.mainloop()
 
@@ -60,21 +60,21 @@ from tkinter import *
 
 root = Tk()
 
-Label(text = 'Имя:').grid(row = 0, column = 0, sticky = W, pady = 10, padx = 10)
+Label(text='Имя:').grid(row=0, column=0, sticky=W, pady=10, padx=10)
 table_name = Entry()
-table_name.grid(row = 0, column = 1, columnspan = 3, sticky = W + E, padx = 10)
+table_name.grid(row=0, column=1, columnspan=3, sticky=W+E, padx=10)
 
-Label(text = 'Столбцов:').grid(row = 1, column = 0, sticky = W, padx = 10, pady = 10)
-table_column = Spinbox(width = 7, from_ = 1, to = 50)
-table_column.grid(row = 1, column = 1, padx = 10)
+Label(text='Столбцов:').grid(row=1, column=0, sticky=W, padx=10, pady=10)
+table_column = Spinbox(width=7, from_=1, to=50)
+table_column.grid(row=1, column=1, padx=10)
 
-Label(text = 'Строк:').grid(row = 1, column = 2, sticky = E)
-table_row  =  Spinbox(width = 7, from_ = 1, to = 100)
-table_row.grid(row = 1, column = 3, sticky = E, padx = 10)
+Label(text='Строк:').grid(row=1, column=2, sticky=E)
+table_row = Spinbox(width=7, from_=1, to=100)
+table_row.grid(row=1, column=3, sticky=E, padx=10)
 
-Button(text = 'Справка').grid(row = 2, column = 0, pady = 10, padx = 10)
-Button(text = 'Вставить').grid(row = 2, column = 2)
-Button(text = 'Отменить').grid(row = 2, column = 3, padx = 10)
+Button(text='Справка').grid(row=2, column=0, pady=10, padx=10)
+Button(text='Вставить').grid(row=2, column=2)
+Button(text='Отменить').grid(row=2, column=3, padx=10)
 
 root.mainloop()
 
