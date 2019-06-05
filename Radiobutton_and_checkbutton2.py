a78e4590231f17654c3e0664c609f215d7b54ed8
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-05 09:54:49 +0500

    Update Radiobutton_and_checkbutton2.py

--- Radiobutton_and_checkbutton2.py
+++ Radiobutton_and_checkbutton2.py
@@ -10,13 +10,13 @@ root = Tk()
 
 cvar1 = BooleanVar()
 cvar1.set(0)
-c1 = Checkbutton(text = "First", variable = cvar1, onvalue = 1, offvalue = 0)
-c1.pack(anchor = W)
+c1 = Checkbutton(text="First", variable=cvar1, onvalue=1, offvalue=0)
+c1.pack(anchor=W)
 
 cvar2 = BooleanVar()
 cvar2.set(0)
-c2 = Checkbutton(text = "Second", variable = cvar2, onvalue = 1, offvalue = 0)
-c2.pack(anchor = W)
+c2 = Checkbutton(text="Second", variable=cvar2, onvalue=1, offvalue=0)
+c2.pack(anchor=W)
 
 root.mainloop()
 
