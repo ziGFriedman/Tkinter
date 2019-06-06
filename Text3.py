315ee4725a259849b9e1ab3c74cabd0a78e99306
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-06 10:06:12 +0500

    Update Text3.py

--- Text3.py
+++ Text3.py
@@ -7,12 +7,12 @@
 from tkinter import *
 root = Tk()
 
-text = Text(width = 50, height = 10)
+text = Text(width=50, height=10)
 text.pack()
 text.insert(1.0, "Hello world!\nline two")
 
 text.tag_add('title', 1.0, '1.end')
-text.tag_config('title', font = ("Verdana", 24, 'bold'), justify = CENTER)
+text.tag_config('title', font=("Verdana", 24, 'bold'), justify=CENTER)
 
 root.mainloop()
 
@@ -24,15 +24,15 @@ root.mainloop()
 from tkinter import *
 
 def smile():
-    label = Label(text = ":)", bg = "yellow")
-    text.window_create(INSERT, window = label)
+    label = Label(text=":)", bg="yellow")
+    text.window_create(INSERT, window=label)
 
 root = Tk()
 
-text = Text(width = 50, height = 10)
+text = Text(width=50, height=10)
 text.pack()
 
-button = Button(text = ":)", command = smile)
+button = Button(text=":)", command=smile)
 button.pack()
 
 root.mainloop()
