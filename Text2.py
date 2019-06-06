7f28bc0f3e5dd6930b9ae42b909307f9a1714da0
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-06 10:06:14 +0500

    Update Text2.py

--- Text2.py
+++ Text2.py
@@ -18,20 +18,20 @@ def deleteText():
 
 root = Tk()
 
-text = Text(width = 25, height = 5)
+text = Text(width=25, height=5)
 text.pack()
 
 frame = Frame()
 frame.pack()
 
-b_insert = Button(frame, text = "Вставить", command = insertText)
-b_insert.pack(side = LEFT)
+b_insert = Button(frame, text="Вставить", command=insertText)
+b_insert.pack(side=LEFT)
 
-b_get = Button(frame, text = "Взять", command = getText)
-b_get.pack(side = LEFT)
+b_get = Button(frame, text="Взять", command=getText)
+b_get.pack(side=LEFT)
 
-b_delete = Button(frame, text = "Удалить", command = deleteText)
-b_delete.pack(side = LEFT)
+b_delete = Button(frame, text="Удалить", command=deleteText)
+b_delete.pack(side=LEFT)
 
 label = Label()
 label.pack()
