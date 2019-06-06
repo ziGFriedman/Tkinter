c7d915883df0dbdddad26904a1b7884c8c8ce3bf
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-06 10:06:05 +0500

    Update Window2.py

--- Window2.py
+++ Window2.py
@@ -21,11 +21,11 @@ def about():
     a.geometry('200x150')
     a['bg'] = 'grey'
     a.overrideredirect(True)
-    Label(a, text = "About this").pack(expand = 1)
+    Label(a, text="About this").pack(expand=1)
     a.after(5000, lambda: a.destroy())
 
-Button(text = "Button", width = 20).pack()
-Label(text = "Label", width = 20, height = 3).pack()
-Button(text = "About", width = 20, command = about).pack()
+Button(text="Button", width=20).pack()
+Label(text="Label", width=20, height=3).pack()
+Button(text="About", width=20, command=about).pack()
 
 root.mainloop()
